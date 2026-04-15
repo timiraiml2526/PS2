2e105b5aa3614dd1796fee1487244bda1e2fb621
timiraiml2526 <timir.aiml2526@gmail.com>
2026-04-15 13:05:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_title="PostureSense",
     page_icon="🧘",
     layout="wide",
-    initial_sidebar_state="auto",
+    initial_sidebar_state="expanded",
 )
 
 # Session defaults
